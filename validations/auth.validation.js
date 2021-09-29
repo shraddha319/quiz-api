59a1b3034a71e0ce3a16bdb748c8b243782a7e90
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-29 15:19:52 +0530

    fix: do not validate password pattern on login

--- validations/auth.validation.js
+++ validations/auth.validation.js
@@ -1,12 +1,11 @@
 const Joi = require('joi');
-const { password } = require('./custom.validation');
 
 // TODO - test input schema
 
 const login = {
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().custom(password),
+    password: Joi.string().required(),
   }),
 };
 
